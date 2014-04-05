02b9957e480b7f6383aa4a224141ccc022b51c01
Evan Murphy <esmurphy7@gmail.com>
2014-04-05 23:55:02 +0000

    commit from nitrous.io

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = 'b60b57ec41732612df54c62183ed4682fe6b6b852cc6c879a4d76a4b2696326ced1f064df1e5c9a4fd4f0d7cfc12a20a5d93d999789f91d8ac1182b0944fd087'
