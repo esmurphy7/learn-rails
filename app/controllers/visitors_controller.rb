bbf1cb4f414b744dcfd2518b0afd993495e8f0bc
Evan Murphy <esmurphy7@gmail.com>
2014-04-08 02:29:31 +0000

    installed zurb foundation and simple_form gems for the front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,15 @@ class VisitorsController < ApplicationController
   def new
     # Any instance variables (@owner) are available to corresponding view files
     @owner = Owner.new
-    # render the view at this path, the .html.erb extension is assumed by default
-    render "visitors/new"
+    # render method is called behind the scenes, renders the view at this path, the .html.erb extension is assumed by default
+    # every page in '/app' uses 'application.html.erb' default layout unless specified otherwise
+    # ie: Can specify one of:
+      # render "visitors/new"
+      # render "visitors/new", layout => false
+      # render "visitors/new", layout => "special"
+    # Flash messages
+      # flash.now[:notice] = 'Welcome!'
+      # flash.now[:alert] = 'My birthday is soon.'
   end
   
 end
--- lib/rubyexample.rb
+++ lib/rubyexample.rb
@@ -1,6 +1,8 @@
 class RubyExample < Object
   
   # Class attributes
+  # ':horriffic' is an immutable variable (symbol)
+  # 'attr_accessor' is shortcut for writing getters and setters
   attr_accessor :honorific
   attr_accessor :name
   attr_accessor :date
@@ -14,17 +16,20 @@ class RubyExample < Object
   def initialize(name,date)
     @name = name
     # '?' indicates that the method will return a boolean value
+    # equivalent to: condition ? value_if_true : value_if_false
     @date = date.nil? ? Date.today : date
   end
   
   def titled_name
+    # Equivalent to: if not @horrific then 'Esteemed'
     @honorific ||= 'Esteemed'
+    # Hashmark indicates expression should be treated as a string
     titled_name = "#{@honorific} #{@name}"
   end
   
   private
   def famous_birthdays
-    # Initialize dictionary data structure
+    # Initialize hash data structure by using curly braces
     birthdays = {
       'Ludwig van Beethoven' => Date.new(1770,12,16),
       'Dave Brubeck' => Date.new(1920,12,6),
@@ -35,9 +40,10 @@ class RubyExample < Object
   
   def december_birthdays
     born_in_december = [ ]
+    # The pipes assign the key and value of the hash to two variables
     for famous_birthdays.each do |name, date|
       if date.month == 12
-        # Insert 'name' attribute into the array
+        # Insert 'name' key into the array
         born_in_december << name
       end
     end
