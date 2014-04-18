88c961fbe7136e639ba18c382999931c8c9982f5
Evan Murphy <esmurphy7@gmail.com>
2014-04-18 00:43:00 +0000

    added functionality to connect and save data to google drive spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,16 @@
 class ContactsController < ApplicationController
   
+  # Display empty contact form
   def new
     @contact = Contact.new
   end
 
+  # Validate and process a submitted form (this method is called when the contact form is submitted)
   def create
     @contact = Contact.new(secure_params)
-    if @contact.valid?
-      # save data and send message
+    if @contact.valid? # Calls Contact model's validation atttributes
+      @contact.update_spreadsheet
+      # TODO: send message
       flash[:notice] = "Message sent from #{@contact.name}"
       redirect_to root_path
     else
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -13,4 +13,21 @@ class Contact < ActiveRecord::Base
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
   
+  # Updates a google drive spreadsheet using Google spreadsheets API from google_drive gem.
+  def update_spreadsheet
+    # Environment variables are used from config/application.yml with help from figaro gem.
+    connection = GoogleDrive.login(ENV["GMAIL_USERNAME"], ENV["GMAIL_PASSWORD"])
+    ss = connection.spreadsheet_by_title("Learn-Rails-Example")
+    if ss.nil?
+      ss = connection.create_spreadsheet("Learn-Rails-Example")
+    end
+    ws = ss.worksheets[0] # Spreadsheets contain multiple worksheets
+    last_row = 1 + ws.num_rows # Get the row after the bottom-most, non-empty row (last_row)
+    ws[last_row, 1] = Time.new # Set row: 'last_row' , column: '1' of worksheet to 'Time.new'
+    ws[last_row, 2] = self.name
+    ws[last_row, 3] = self.email
+    ws[last_row, 4] = self.content
+    ws.save
+  end
+  
 end
